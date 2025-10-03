84e7609e773dd9d928bea1e9371fd74812a784a8
muthusuganthan <muthusuganthan2020pmk@gmail.com>
2025-10-03 11:39:01 +0530

    course project3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 app.use(express.json());
 const prisma = new PrismaClient();
 const { z, ZodError } = require("zod");
